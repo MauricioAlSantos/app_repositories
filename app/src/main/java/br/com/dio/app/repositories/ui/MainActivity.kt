9436ba03a43a19aca99df69ed9064408016d1b47
MauricioAlSantos <mauricioalsantos@gmail.com>
2021-09-25 07:59:14 -0300

    initial

--- app/src/main/java/br/com/dio/app/repositories/ui/MainActivity.kt
+++ app/src/main/java/br/com/dio/app/repositories/ui/MainActivity.kt
@@ -4,20 +4,55 @@ import android.os.Bundle
 import android.util.Log
 import android.view.Menu
 import android.view.MenuItem
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.SearchView
 import br.com.dio.app.repositories.R
 import br.com.dio.app.repositories.databinding.ActivityMainBinding
+import br.com.dio.app.repositories.presentation.MainViewModel
+import org.koin.androidx.viewmodel.ext.android.viewModel
 
-class MainActivity : AppCompatActivity() {
-
+class MainActivity : AppCompatActivity(), SearchView.OnQueryTextListener {
+    private val viewModel by viewModel<MainViewModel>()
     private val binding by lazy { ActivityMainBinding.inflate(layoutInflater) }
-
+    private val adapter by lazy {RepoListAdapter()}
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
 
         setSupportActionBar(binding.toolbar)
+        binding.rvRepos.adapter=adapter
+        viewModel.repos.observe(this){
+            when(it){
+                is MainViewModel.State.Error -> {
+                    Toast.makeText(this, "Sem resultados", Toast.LENGTH_SHORT).show()
+                }
+                MainViewModel.State.Loading -> TODO()
+                is MainViewModel.State.Sucess -> {
+                    adapter.submitList(it.list)
+                }
+            }
+        }
     }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        menuInflater.inflate(R.menu.main_menu,menu)
+        val searchView = menu.findItem(R.id.action_search).actionView as SearchView
+        searchView.setOnQueryTextListener(this)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onQueryTextSubmit(query: String?): Boolean {
+        if (query != null) {
+            viewModel.getRepoList(query)
+            return true
+        }
+        return false
+    }
+
+    override fun onQueryTextChange(newText: String?): Boolean {
+        return false
+    }
+
 
 }
